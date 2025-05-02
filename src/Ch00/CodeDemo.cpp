8d8282e68933bd76ac92eac19fd601e113d351d0
JoshLiu111 <joshua1lxy@gmail.com>
2025-05-02 16:03:55 -0700

    update

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 00_03
+// Exercise 00_03 with modification
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
@@ -11,7 +11,7 @@ int main(){
     std::cin >> num_1;
     std::cout << "Enter number 2: " << std::flush;
     std::cin >> num_2;
-    
+
     result = num_1 + num_2;
     
     std::cout << "The result of the addition is " << result << std::endl;
