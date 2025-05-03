57d433ce0cdfdfecb8c02fbe9d78972a125b234b
JoshLiu111 <joshua1lxy@gmail.com>
2025-05-02 17:12:49 -0700

    submit

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,15 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+//print and read
+#include<iostream>
+
+
+int main() {
+    std::cout << "Hi There!" << std::endl;
+    //create blind line
+    std::cout << std::endl << std::endl;
+    return 0;
+}
+
